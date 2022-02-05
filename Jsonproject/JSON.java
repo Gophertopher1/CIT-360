20c5a1bc7ed112f570d03206c81e2cf1ff1b58e3
Gophertopher1 <mcgill.chris1@gmail.com>
2022-02-05 12:49:50 -0700

    update

---
+++ Jsonproject/JSON.java
@@ -0,0 +1,31 @@
+package Jsonproject;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.*;
+
+
+public class JSON {
+    public static void main(String[] args){
+        try {
+            JSONParser userParser = new JSONParser();
+            JSONObject userJson = (JSONObject) userParser.parse(new FileReader("src/main/user.json"));
+            
+            System.out.println("User JSON: \n" + userJson + "\n");
+            
+            userJson.put("class", "CIT 360");
+            
+            System.out.println("Modified User JSON: \n" + userJson);
+            
+            System.out.println("\nLoad JSON:");
+            System.out.println("user_id: " + userJson.get("user_id"));
+            System.out.println("last_name: " + userJson.get("last_name"));
+            System.out.println("first_name: " + userJson.get("first_name"));
+            System.out.println("username: " + userJson.get("username"));
+            System.out.println("class: " + userJson.get("class"));
+        } 
+        catch (IOException | ParseException e) {
+        }
+    }
+}
